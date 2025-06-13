de66ecfd6c3e3ff25073054933c0ae995592e44b
mmanci <137300963+mmanci@users.noreply.github.com>
2025-06-13 15:09:36 -0400

    Update points_within.cpp

--- src/points_within.cpp
+++ src/points_within.cpp
@@ -1,6 +1,13 @@
 #include <R.h>
 #include <Rinternals.h>
+
+// Undefine the 'length' macro to prevent conflicts
+#undef length
+
 #include <set>
+#include <vector>
+#include <functional>
+
 using namespace std;
 
 /* -- points_witin from the spp package
